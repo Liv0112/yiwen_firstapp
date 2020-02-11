17989ffb07ff0e6f8136e22aa6509ed01d97a93c
Liv0112 <yw0112wang@gmail.com>
2020-02-11 16:04:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/yiwen_firstapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.yiwen_firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
